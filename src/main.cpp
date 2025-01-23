4fd4ed1677f97fe8bbb6a9b8edbf860c9254e099
00cedke <mathiskera2509@gmail.com>
2025-01-23 17:35:51 +0100

    Added Wi-Fi Status and added version.

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,59 @@
 #include <string>
 #include <stdio.h>
 #include "frda.h"
+#include "version.h"
+
+#define ESCAPE_GO_BACK_1_LINE "\x1b[1A"
 
 #define NINTENDO_TEXT CONSOLE_YELLOW "Nintendo" CONSOLE_CYAN
 #define NEXTENDO_TEXT CONSOLE_MAGENTA "Nextendo" CONSOLE_CYAN
 
+Result checkInternetConnection() {
+    u32 wifiStatus = 0;
+    Result rc = ACU_GetWifiStatus(&wifiStatus);
+
+    if (R_FAILED(rc) || wifiStatus == 0) {
+        return rc;
+    }
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
-    frdaInit();
     gfxInitDefault();
     PrintConsole bottomConsole;
     consoleInit(GFX_BOTTOM, &bottomConsole);
 
-    // Set Client SDK Version
+    Result rc = frdaInit();
+    if (R_FAILED(rc))
+    {
+        printf(CONSOLE_RED "Failed to initialize FRDA: 0x%08lX\n", rc);
+        svcSleepThread(3000000000);
+        gfxExit();
+        return 0;
+    }
+
+    rc = checkInternetConnection();
+    if (R_FAILED(rc)) {
+        printf("Wi-Fi connection error: 0x%08lX\n", rc);
+        svcSleepThread(3000000000);
+        gfxExit();
+        return 0;
+    }
+
     FRDA_SetClientSdkVersion(0x70000C8);
     FRDA_Save();
-    FRDA_UnloadLocalAccount();
 
-    std::string serverName = "Nintendo";
+    rc = FRDA_UnloadLocalAccount();
+    if (R_FAILED(rc))
+    {
+        printf(CONSOLE_RED "Failed to unload local account: 0x%08lX\n", rc);
+        svcSleepThread(3000000000);
+        gfxExit();
+        return 0;
+    }
+
     u8 accountId = 1;
     NASType nastype = NAS_LIVE;
     NASEnvironment nasenv = NAS_ENV_L;
@@ -30,42 +66,43 @@ int main(int argc, char *argv[])
     printf(CONSOLE_CYAN "== Nextendo Account Switcher ==\n" CONSOLE_RESET);
     printf("\n");
     printf(CONSOLE_CYAN "Please select a network:\n" CONSOLE_RESET);
-    printf(CONSOLE_CYAN "Press A to switch to " NINTENDO_TEXT " Account.\n" CONSOLE_RESET);
+    printf(CONSOLE_CYAN "Press Y to switch to " NINTENDO_TEXT " Account.\n" CONSOLE_RESET);
     printf(CONSOLE_CYAN "Press X to switch to " NEXTENDO_TEXT " Account.\n" CONSOLE_RESET);
     printf("\n");
     printf(CONSOLE_CYAN "Press START to exit.\n" CONSOLE_RESET);
+    printf("\n");
+    printf("\n");
+    printf(CONSOLE_CYAN "Account Switcher Version: %s\n", VERSION CONSOLE_RESET);
 
     while (aptMainLoop())
     {
         gspWaitForVBlank();
-	gfxSwapBuffers();
-	hidScanInput();
+	    gfxSwapBuffers();
+	    hidScanInput();
 
         u32 kDown = hidKeysDown();
 
-        if (kDown & KEY_A)
+        if (kDown & KEY_Y)
         {
-            serverName = "Nintendo";
             accountId = 1;
-            nastype = NAS_LIVE; // Switch to nasc live/prod url
+            nastype = NAS_LIVE;
             nasenv = NAS_ENV_L;
             FRDA_LoadLocalAccount(accountId);
             printf("\n");
-            printf(CONSOLE_CYAN " Patching " NINTENDO_TEXT " network.." CONSOLE_RESET);
+            printf(CONSOLE_CYAN " Patching " NINTENDO_TEXT " network..", ESCAPE_GO_BACK_1_LINE CONSOLE_RESET);
             svcSleepThread(3000000000);
             break;
         }
 
         if (kDown & KEY_X)
         {
-            serverName = "Nextendo";
             accountId = 2;
-            nastype = NAS_TEST; // Switch to nasc test url
+            nastype = NAS_TEST;
             nasenv = NAS_ENV_L;
             FRDA_CreateLocalAccount(accountId, nastype, nasenv, 1);
             FRDA_LoadLocalAccount(accountId);
             printf("\n");
-            printf(CONSOLE_CYAN " Patching " NEXTENDO_TEXT " network.." CONSOLE_RESET);
+            printf(CONSOLE_CYAN " Patching " NEXTENDO_TEXT " network..", ESCAPE_GO_BACK_1_LINE CONSOLE_RESET);
             svcSleepThread(3000000000);
             break;
         }
@@ -73,7 +110,7 @@ int main(int argc, char *argv[])
         if (kDown & KEY_START)
         {
             printf("\n");
-            printf(CONSOLE_RED "Exiting..\n" CONSOLE_RESET);
+            printf(CONSOLE_RED "Exiting..\n", ESCAPE_GO_BACK_1_LINE CONSOLE_RESET);
             svcSleepThread(3000000000);
             break;
         }
---
+++ src/version.h
@@ -0,0 +1,6 @@
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#define VERSION CONSOLE_BLUE "v1.1.0" CONSOLE_CYAN
+
+#endif
